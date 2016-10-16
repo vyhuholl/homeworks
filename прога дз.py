4ff33d561bcace148408364c6da7740ee6519f91
vyhuholl <olga-p-98@mail.ru>
2016-10-16 23:32:18 +0300

    Add files via upload

---
+++ прога дз.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a / b == c:
+    print('a поделить на b равно c')
+else:
+    print('a поделить на b не равно c')
+if a ** b == c:
+    print('a в степени b равно c')
+else:
+    print('a в степени b не равно c')
