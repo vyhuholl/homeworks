9f962f484ee521cd9f64677cf16ec01d2eab1833
vyhuholl <olga-p-98@mail.ru>
2017-04-03 14:10:48 +0300

    Add files via upload

--- frequency.py
+++ frequency.py
@@ -1,13 +1,14 @@
+import csv
+
 def text_process(text_name):
     f = open(text_name, 'r', encoding='utf-8')
     text = f.read()
     l = text.split()
     l1 = []
     for word in l:
-        l1.append(word.strip('.,;:?![]{}'))
+        l1.append(word.strip('.,;:?!'))
     f.close()
     return l1
-
 
 def freq_dict(text):
     dictionary = {}
@@ -17,11 +18,15 @@ def freq_dict(text):
             if i == word:
                 freq += 1
         dictionary[word] = freq
-    return dictionary
-
+    return sorted(dictionary)
 
 def main():
-    text = text_process(text.txt)
+    text = text_process('text.txt')
     dictionary = freq_dict(text)
-    word = input()
-    print(dictionary[word])
+    text2 = open('text2.tsv', 'w')
+    for word in dictionary:
+        text2.write(word + '\t' + str(dictionary[word]))
+    text2.close()
+
+main()
+    
