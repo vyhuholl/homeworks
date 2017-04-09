412d8e44d3e6bef7b18183f4c56eb1d6ed105f1a
vyhuholl <olga-p-98@mail.ru>
2017-04-09 21:54:33 +0300

    Update hw15.py

--- hw15/hw15.py
+++ hw15/hw15.py
@@ -1,27 +1,27 @@
 def new_sentence(sentence):
-sentence1 = ''
-for word in sentence:
-sentence1 += word.strip('.,;:?!') + ' '
-sentence1 += '.'
-return sentence1
+  sentence1 = ''
+  for word in sentence:
+    sentence1 += word.strip('.,;:?!') + ' '
+    sentence1 += '.'
+    return sentence1
 
 def text_process(text_name):
-f = open(text_name, 'r', encoding='utf-8')
-text = f.read()
-text = text.replace('!','.')
-text = text.replace('?', '.')
-text = text.replace('...','.')
-l = text.split(.)
-l1 = [new_sentence(sentence) for sentence in text]
-f.close()
-return l1
+  f = open(text_name, 'r', encoding='utf-8')
+  text = f.read()
+  text = text.replace('!','.')
+  text = text.replace('?', '.')
+  text = text.replace('...','.')
+  l = text.split(.)
+  l1 = [new_sentence(sentence) for sentence in text]
+  f.close()
+  return l1
 
 
 def create_dict(text):
-dictionary = {sentence: {word: len(word) for word in sentence} for sentence in text}
+  dictionary = {sentence: {word: len(word) for word in sentence} for sentence in text}
 
 def main():
-text = text_process('text.txt')
-return(create_dict(text))
+  text = text_process('text.txt')
+  return(create_dict(text))
 
 main()
