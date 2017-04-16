2b08730145eefdb0df770fb0aad805bab2afce92
vyhuholl <olga-p-98@mail.ru>
2017-04-16 13:49:18 +0400

    Create hw16.py

---
+++ hw16/hw16.py
@@ -0,0 +1,18 @@
+import os
+
+
+def symbols(s):
+ans = True
+for i in s:
+if i not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz':
+ans = False
+return ans
+
+def main():
+n = 0
+for f in os.listdir('.'):
+if symbols(f) = True:
+n += 1
+print (f)
+print (n)
+main()
