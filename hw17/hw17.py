6272dc1e17a3b7e6c918562ef6013c57400b70fd
vyhuholl <olga-p-98@mail.ru>
2017-04-26 13:12:35 +0300

    Update hw17.py

--- hw17/hw17.py
+++ hw17/hw17.py
@@ -1,6 +1,27 @@
 import os
 
-file_tree = os.walk('.')
-l = []
-for f in file_tree:
-  l.append(f)
+def create_letters_list():
+  file_tree = os.walk('.')
+  letters = {}
+  for d in file_tree:
+    folder_name = d[0].strip('.\/')
+    letter = folder_name[0]
+    if letter in letters:
+      letters[letter] += 1
+    else:
+      letters[letter] = 1
+   return letters
+
+
+def main():
+  letters = create_letters_list
+  letter = ''
+  n = 0
+  for i in letters:
+    if letters[i] > n:
+      letter = i
+      n = letters[i]
+  print(letter)
+
+
+main()
