b745c7377f19ae8f4f8263886319f6519161663b
vyhuholl <olga-p-98@mail.ru>
2017-09-30 01:00:08 +0300

    Update прога дз корпус.py

--- прога дз корпус.py
+++ прога дз корпус.py
@@ -14,21 +14,20 @@ def download_page(pageUrl):
     except:
         return('Error')
 
-commonUrl = 'http://www.marpravda.ru/news/?day='
+commonUrl = 'http://www.marpravda.ru/'
 regPostTitle = re.compile('<div class=\"news_name\"><a href=\"(.*?)\">(.*?)</a></div>', flags= re.DOTALL)
 regPostTopic = re.compile('<span class=\"rubric\"><a href=\"(.*?)\">(.*?)</a>', flags= re.DOTALL)
 regPostAuthor = re.compile('<a href=\"javascript:void\(0\);\">(.*?)</a>', flags= re.DOTALL)
 regAuthor1 = re.compile('<a href=\"javascript:void\(0\);\">', flags= re.DOTALL)
 regAuthor2 = re.compile('</a>', flags= re.DOTALL)
+regArticle = re.compile('<article>(.*?)</article>')
 regTag = re.compile('<.*?>', re.DOTALL)
-regScript = re.compile('<script>.*?</script>', re.DOTALL)
-regComment = re.compile('<!--.*?-->', re.DOTALL)
 
 for year in range(2016, 2018):
     for month in range(1, 13):
         for day in range(1, 32):
             date = str(day) + '.' + str(month) + '.' + str(year)
-            pageUrl = commonUrl + date
+            pageUrl = commonUrl + 'news/?day=' + date
             page = download_page(pageUrl)
             if page != 'Error':
                 path = 'C:/homework' + os.sep + str(year) + os.sep + str(month) + os.sep + str(day) + os.sep
@@ -48,6 +47,30 @@ for year in range(2016, 2018):
                 topics = []
                 for i in t1:
                     topics.append(i[1])
+                for i in range(len(titles)):
+                    header = titles[i]
+                    link = links[i]
+                    topic = topics[i]
+                    text = download_page(commonUrl + link)
+                    author = (regPostAuthor.search(text)).group()
+                    author = regAuthor1.sub('', author)
+                    author = regAuthor2.sub('', author)
+                    text = (regArticle.search(text)).group()
+                    text = regTag.sub('', text)
+                    file_path = path + 'not mystem/' + header + '.txt'
+                    file = open(file_path, 'w')
+                    file.write('@au' + author + '\n')
+                    file.write('@ti' + header + '\n')
+                    file.write('@da' + date + '\n')
+                    file.write('@topic' + topic + '\n')
+                    file.write('@url' + link + '\n')
+                    file.write(text)
+                    writer.writerow([file_path, author, '', '', header, date, 'публицистика', '', '', topic, '', 'нейтральный', 'н-возраст', 'н-уровень', 'республиканская', link, 'Марийская правда', '', year, 'газета', 'Россия', 'республика Марий-Эл', 'ru'])
+                inp = path + 'not mystem/'
+                lst = os.listdir(inp)
+                for fl in lst:
+                    os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem XML" + os.sep + fl + '-- xml')
+                    os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem plain text" + os.sep + fl)
                 for i in range(len(titles)):
                     header = titles[i]
                     link = links[i]
