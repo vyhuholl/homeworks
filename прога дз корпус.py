a1dd2f4f19433737d62e9cbcdc6f83b0e7a72fa1
vyhuholl <olga-p-98@mail.ru>
2017-09-29 22:39:34 +0300

    Update прога дз корпус.py

--- прога дз корпус.py
+++ прога дз корпус.py
@@ -8,7 +8,7 @@ import csv
 def download_page(pageUrl):
     try:
         page = urllib.request.urlopen(pageUrl)
-        text = page.read().decode('ISO-8859-1')
+        text = page.read().decode('utf-8')
         time.sleep(2)
         return text
     except:
@@ -16,9 +16,17 @@ def download_page(pageUrl):
 
 commonUrl = 'http://www.marpravda.ru/news/?day='
 regPostTitle = re.compile('<div class="news_name"><a href=".*">.*</a></div>', flags= re.DOTALL)
+regTitle1 = re.compile('<div class="news_name"><a href=".*">', flags= re.DOTALL)
+regTitle2 = re.compile('</a></div>', flags= re.DOTALL)
 regPostLink = re.compile('<a class="left" href=".*">Читать далее</a>', flags= re.DOTALL)
+regLink1 = re.compile('<a class="left" href="', flags= re.DOTALL)
+regLink2 = re.compile('">Читать далее</a>', flags= re.DOTALL)
 regPostTopic = re.compile('<span class="rubric"><a href=".*">.*</a>', flags= re.DOTALL)
+regTopic1 = re.compile('<span class="rubric"><a href=".*">', flags= re.DOTALL)
+regTopic2 = re.compile('</a>', flags= re.DOTALL)
 regPostAuthor = re.compile('Автор статьи: <br /> <a href="javascript:void(0);">.*</a>', flags= re.DOTALL)
+regAuthor1 = re.compile('Автор статьи: <br /> <a href="javascript:void(0);">', flags= re.DOTALL)
+regAuthor2 = re.compile('</a>', flags= re.DOTALL)
 regTag = re.compile('<.*?>', re.DOTALL)
 regScript = re.compile('<script>.*?</script>', re.DOTALL)
 regComment = re.compile('<!--.*?-->', re.DOTALL)
@@ -40,29 +48,29 @@ for year in range(2016, 2018):
                 titles = regPostTitle.findall(page)
                 new_titles = []
                 for t in titles:
-                    clean_t = regTag.sub("", t)
+                    clean_t = regTitle1.sub("", t)
+                    clean_t = regTitle2.sub("", clean_t)
                     new_titles.append(clean_t)
                 links = regPostLink.findall(page)
                 new_links = []
                 for l in links:
-                    clean_l = re.search('"/news/.*"', l)
-                    clean_l = clean_l.strip('"')
+                    clean_l = regLink1.sub("", l)
+                    clean_l = regLink2.sub("", clean_l)
                     new_links.append(clean_l)
                 topics = regPostTopic.findall(page)
                 new_topics = []
                 for t in topics:
-                    clean_t = regTag.sub("", t)
+                    clean_t = regTopic1.sub("", t)
+                    clean_t = regTopic2.sub("", clean_t)
                     new_topics.append(clean_t)
-                print(new_titles)
-                print(new_links)
-                print(new_topics)
                 for i in range(len(titles)):
                     header = new_titles[i]
                     link = new_links[i]
                     topic = new_topics[i]
                     text = download_page(link)
                     author = regPostAuthor(text)
-                    clean_author = regTag.sub('', author)
+                    clean_author = regAuthor1.sub('', author)
+                    clean_author = regAuthor2.sub('', clean_author)
                     text = regTag.sub('', text)
                     text = regScript.sub('', text)
                     text = regComment.sub('', text)
@@ -70,6 +78,16 @@ for year in range(2016, 2018):
                     file = open(file_path, 'w')
                     file.write('@au' + author + '\n')
                     file.write('@ti' + header + '\n')
+                    file.write('@da' + date + '\n')
+                    file.write('@topic' + topic + '\n')
+                    file.write('@url' + link + '\n')
+                    file.write(text)
+                    writer.writerow([file_path, clean_author, '', '', header, date, 'публицистика', '', '', topic, '', 'нейтральный', 'н-возраст', 'н-уровень', 'республиканская', link, 'Марийская правда', '', year, 'газета', 'Россия', 'республика Марий-Эл', 'ru'])
+                inp = path + 'not mystem/'
+                lst = os.listdir(inp)
+                for fl in lst:
+                    os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem XML" + os.sep + fl + '-- xml')
+                    os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem plain text" + os.sep + fl)
                     file.write('@da' + date + '\n')
                     file.write('@topic' + topic + '\n')
                     file.write('@url' + link + '\n')
