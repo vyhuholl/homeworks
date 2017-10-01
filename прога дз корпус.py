c45e3745d4e52c3991d3403654c782232b5bb50f
vyhuholl <olga-p-98@mail.ru>
2017-10-01 18:57:49 +0300

    Update прога дз корпус.py

--- прога дз корпус.py
+++ прога дз корпус.py
@@ -38,6 +38,7 @@ for year in range(2016, 2018):
                 for i in t:
                     links.append(i[0])
                     title = re.sub('[\\\\/:*?"<>|]', '', i[1])
+                    title = title.replace('\ufeff', '')
                     titles.append(title)
                 t1 = regPostTopic.findall(page)
                 topics = []
