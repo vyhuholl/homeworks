4c3f3b6aa5db371b38c02325c25bedc8fd8c4aba
vyhuholl <olga-p-98@mail.ru>
2017-12-02 11:30:29 +0300

    Update additional_hw.py

--- additional_hw/additional_hw.py
+++ additional_hw/additional_hw.py
@@ -1,6 +1,15 @@
 from flask import request
 from flask import render_template
 from pymystem3 import Mystem
+import urllib.request
+
+def download_page(pageUrl):
+    try:
+        page = urllib.request.urlopen(pageUrl)
+        text = page.read().decode('utf-8')
+        return text
+    except:
+        return('Error')
 
 m = Mystem()
 
