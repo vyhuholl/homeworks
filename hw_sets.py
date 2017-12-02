97a3926f2877fed4ad4c7a733b9489907dd51fdb
vyhuholl <olga-p-98@mail.ru>
2017-12-02 12:08:59 +0300

    Update hw_sets.py

--- hw_sets.py
+++ hw_sets.py
@@ -7,8 +7,19 @@ def download_page(pageUrl):
         return text
     except:
         return('Error')
-  
+
+regTag = re.compile('<.*?>', flags= re.DOTALL)
 links=['http://www.rosbalt.ru/style/2016/11/29/1571387.html', 'https://wek.ru/na-marse-nashli-zagadochnyj-labirint', 'https://www.kp.ru/daily/26612/3630125/', 'https://mir24.tv/news/15371682', 'https://www.m24.ru/articles/nauka/29112016/123422']
 texts = []
-for i in link:
-  
+for i in links:
+    page = download_page(i)
+    page = regTag.sub('', page)
+    texts.append(set(page))
+
+with open('ans_1.txt', 'w') as ans_1:
+    ans_1.write(texts[0] | texts[1] | texts[2] | texts[3] | texts[4])
+    ans_1.close()
+
+with open('ans_2.txt', 'w') as ans_2:
+    ans_2.write(texts[0] ^ texts[1] ^ texts[2] ^ texts[3] ^ texts[4])
+    ans_2.close()
