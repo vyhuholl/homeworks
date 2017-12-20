4580a753cdb49c1a1ee338d7631092284f374e0f
vyhuholl <olga-p-98@mail.ru>
2017-12-20 22:49:31 +0300

    Update additional_hw.py

--- additional_hw/additional_hw.py
+++ additional_hw/additional_hw.py
@@ -16,7 +16,15 @@ def download_page(pageUrl):
         return text
     except:
         return('Error')
-
+    
+def lemmatize(text):
+    
+    
+def clean_text(text):
+    
+def macedonian_weather():
+    
+    
 with open('dictionary.txt', 'w') as dictionary:
     common_url = 'http://www.dorev.ru/ru-index.html?xmmpoll='
     letters = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'э', 'ю', 'я']
@@ -26,6 +34,11 @@ with open('dictionary.txt', 'w') as dictionary:
     
 app = Flask(__name__)
 
+@app.route('127.0.0.1/')
+def index():
+    weather = macedonian_weather()
+    return render_template('index.html', weather = weather)
+
 @app.route('127.0.0.1/<name>')
 def main_page(name=None):
     return render_template('main_page.html', name=name)
@@ -33,8 +46,13 @@ def main_page(name=None):
 @app.route('127.0.0.1/news')
 def news_page(name=None):
     meduza = download_page('https://meduza.io')
-    meduza_clean = regTag.sub('', meduza)
-    return render_template('news_page.html', name=name)
+    file = open('input.txt','w', encoding = 'utf-8')
+    clean_meduza = clean_text(meduza)
+    clean_text = find_text(text)
+    file.write(clean_meduza)
+    file.close()
+    clean_meduza = lemmatize()
+    return render_template('news_page.html', clean_meduza = clean_meduza)
 
 @app.route('127.0.0.1/test')
 def test_page(name=None):
